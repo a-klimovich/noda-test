84d843bb48121c758308f4a307eca12a473b6e03
Artur <ar2r.klimovich@gmail.com>
2023-11-16 16:26:38 +0100

    add: mixin + footer draft

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  private assetsPath = '../../../assets/svg/'
 
+  navData = [
+    {
+      title: 'Products',
+      list: [
+        { text: 'Open Banking Payments', url: '#' },
+        { text: 'Card Payments', url: '#' },
+        { text: 'Checkout Form', url: '#' },
+        { text: 'Bank Payouts', url: '#' },
+        { text: 'Card Payouts', url: '#' },
+        { text: 'Financial AI Toolset', url: '#' },
+        { text: 'Sign in via Bank', url: '#' },
+      ],
+    },
+    {
+      title: 'Developers',
+      list: [
+        { text: 'Noda Pay tech documentation', url: '#' },
+        { text: 'Open-Banking Demo', url: '#' },
+        {
+          text: 'Log In To Hub',
+          url: '#',
+          icon: this.assetsPath + 'logo_hub.svg',
+        },
+      ],
+    },
+    {
+      title: 'Pricing',
+      list: [{ text: 'Pricing', url: '#' }],
+    },
+  ];
 }
